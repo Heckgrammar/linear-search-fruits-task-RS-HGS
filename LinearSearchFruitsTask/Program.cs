dbb04efee3bd2087a5c365e43e8185d6c54d758e
RS-HGS <144434850+RS-HGS@users.noreply.github.com>
2025-02-05 21:32:16 +0000

    Update Program.cs
    done

--- LinearSearchFruitsTask/Program.cs
+++ LinearSearchFruitsTask/Program.cs
@@ -1,11 +1,24 @@
-﻿namespace LinearSearchFruitsTask
+namespace LinearSearchFruitsTask
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            string[] fruits = { "banana", "apple", "orange", "pear", "grape", "pineapple" };
-
+            string[] fruits = { "banana", "apple", "orange", "pear", "grape", "pineapple" }; //fruits array
+            bool wordFound = false; //boolean for whether the word has been found (which will be our output)
+            Console.WriteLine("What word would you like to find?"); //prompts user
+            string wordToFind = Console.ReadLine(); //assigns user input to a string
+            foreach (string f in fruits) //checks against every index in the fruits array
+            {
+                if (f == wordToFind) //compares the user input to the index within the fruits array
+                {
+                     wordFound = true; //if the index matches, the word has been found so the boolean is set to true
+                     break; //breaks out of array, as the word has already been found
+                }
+            }
+            Console.WriteLine(wordFound); //outputs True if the word is found and False if it isnt
+            
+            //////////////////////////////////////////////////////////////////////////////////////
             // Your program should:
             // Ask the user what word they would like to find
             // Output the message True if the word is found
